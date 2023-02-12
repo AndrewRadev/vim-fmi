d25dfaafb0f238bf7c499492f83d53f137831438
Andrew Radev <andrey.radev@gmail.com>
2023-02-12 20:55:03 +0200

    Set up sentry for error tracking

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'simple_form'
 gem 'rdiscount'
 gem 'sanitize'
 gem 'rails_autolink'
+gem 'sentry-ruby'
+gem 'sentry-rails'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
---
+++ config/initializers/sentry.rb
@@ -0,0 +1,13 @@
+Sentry.init do |config|
+  config.dsn = 'https://0a42739d26424637a138f5d44c0c923b@o4504668591161344.ingest.sentry.io/4504668594241536'
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |context|
+    true
+  end
+end
