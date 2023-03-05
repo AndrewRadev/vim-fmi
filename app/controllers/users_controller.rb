41629f6720f2f7e27f17d63aa4ac1da64beaf768
Andrew Radev <andrey.radev@gmail.com>
2023-03-05 19:25:44 +0200

    Fix task issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
-    @done_task_count = @user.tasks.count
+    @done_task_count = @user.solutions.completed.map(&:task).uniq.count
     @total_task_count = Task.count
   end
 end
