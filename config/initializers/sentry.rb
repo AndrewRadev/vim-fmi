72cfcb57547c9a25f431f6c48eabdacaec890d3e
Andrew Radev <andrey.radev@gmail.com>
2023-03-06 21:42:49 +0200

    Disable sentry transactions

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -8,9 +8,9 @@ Sentry.init do |config|
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production.
-  config.traces_sample_rate = 1.0
+  config.traces_sample_rate = 0.0
   # or
   config.traces_sampler = lambda do |context|
-    true
+    false
   end
 end
