4243265873af33939cc24fff799f850991e9cb58
Andrew Radev <andrey.radev@gmail.com>
2023-03-19 16:43:20 +0200

    Use Sofia timezone

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module VimFmi
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Europe/Sofia"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
